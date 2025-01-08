f69ce5697f8075eaa71cb4d38df00615751ddd06
kanghy <102237457+icedisk2009@users.noreply.github.com>
2025-01-08 09:18:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ st.title('나의 첫번째 앱')
 st.text('\n\n')
 st.write('안녕하세요. 저는 🍊🌵입니다')
 st.write('이메일주소: t002@daejin.sen.hs.kr')
+
+
+Function signature[source]
+st.button(label, key=None, help=None, on_click=None, args=None, kwargs=None, *, type="secondary", icon=None, disabled=False, use_container_width=False)
